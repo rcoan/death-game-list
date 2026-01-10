7ec95a0111f52bdeef86564dded33692c1e937ad
rcoan <rvc.coan@gmail.com>
2026-01-10 11:18:23 -0300

    add login to see pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  before_action :authenticate_user!
   before_action :set_current_year
 
   private
