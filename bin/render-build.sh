055250e95da75c05843fb183ad7f571f716c2e72
rcoan <rvc.coan@gmail.com>
2026-01-10 12:16:49 -0300

    add new command

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,4 +8,5 @@ bundle exec rails assets:clean
 
 # Run migrations (will be skipped if already up to date)
 bundle exec rails db:migrate RAILS_ENV=production
+bundle exec rails db:seed RAILS_ENV=production
 
